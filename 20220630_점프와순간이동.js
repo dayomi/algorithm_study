286a8ec5010fe2a6df9720450338fea9785f0204
dayomi <rkdek6826@gmail.com>
2022-06-30 23:05:31 +0900

    Create 20220630_점프와순간이동.js

---
+++ 20220630_점프와순간이동.js
@@ -0,0 +1,19 @@
+function solution(n)
+{
+    var ans = 0;
+    
+    calculate(n);
+    
+    function calculate(num){
+        if(num == 0)
+            return;
+        else if (num % 2 == 0)
+            calculate(num / 2);
+        else{
+            ans ++;
+            calculate(num - 1);
+        }
+    }
+
+    return ans;
+}
