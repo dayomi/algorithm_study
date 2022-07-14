a85dc46efd28bb582e0d97e3822516dd0f8890f5
dayomi <rkdek6826@gmail.com>
2022-07-14 23:19:52 +0900

    Create 20220714_n진수게임.js
    https://school.programmers.co.kr/learn/courses/30/lessons/17687

---
+++ 20220714_n진수게임.js
@@ -0,0 +1,23 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/17687
+
+function solution(n, t, m, p) {
+    var answer = '', all = '';
+    
+    // m*t 까지 부르는 문장 다 구하기
+    for(var i=0; i<=m*t; i++)
+        all += i.toString(n);;
+    
+    // 대문자로 변환
+    all = all.toUpperCase(); 
+    
+    // 튜브가 불러야하는 숫자 찾기
+    for(var i=0; i<all.length; i++){
+        if((i % m) + 1 == p){
+            answer += all[i];
+            if(answer.length == t)
+                break;
+        }
+    }
+    
+    return answer;
+}
